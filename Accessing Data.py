29ff699a4bfae685ef84cd0aa8a75ea368f76ad3
Itoro Usa <169055253+Kosher-T@users.noreply.github.com>
2025-06-21 19:13:11 +0100

    Update Accessing Data.py

--- Accessing Data.py
+++ Accessing Data.py
@@ -1,7 +1,10 @@
 import pandas as pd
+
+
 def specific_returner(file_name, value):
     # Write code here
     df = pd.read_csv(file_name)
+    
     try:
         value = int(value)
         row = df.iloc[value]
