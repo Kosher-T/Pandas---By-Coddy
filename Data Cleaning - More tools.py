513c60235e4ed05e2fae92be791a1d270327aff6
Itoro Usa <169055253+Kosher-T@users.noreply.github.com>
2025-06-22 18:05:59 +0100

    Update Data Cleaning - More tools.py

--- Data Cleaning - More tools.py
+++ Data Cleaning - More tools.py
@@ -1,7 +1,12 @@
 # pandas as pd is already imported
+
+
 df = pd.read_csv("./missing.csv")
+
 # Write your code below
 df = df.drop_duplicates()
+
 for column in df.columns:
     df = df.rename(columns={f"{column}": column.lower()})
+
 df["is_valid"] = df["is_valid"].astype(bool)
