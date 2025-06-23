9b0d042b2bb5c13edf677464ba885d253865cff1
Itoro Usa <169055253+Kosher-T@users.noreply.github.com>
2025-06-23 21:10:16 +0100

    Update and rename Filter Data to Filter Data.py

--- Filter Data.py
+++ Filter Data.py
@@ -1,5 +1,8 @@
 # pandas as pd is already imported
+
+
 df = pd.read_csv("./stats.csv")
+
 # Write your code below
 df = df[df["SKILL_POINTS"] > 7]
 df = df[(df["UTILIZATION"] <= 0.7)|(df["UTILIZATION"] <= 0.7)]
